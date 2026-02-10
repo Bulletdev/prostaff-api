230e03aea58aaf0942db829132c16da85170ad92
Michael D. <contato@michaelbullet.com>
2026-02-10 18:06:34 -0300

    fix: correct namespace for VodTimestampsController module

--- app/modules/vod_reviews/controllers/vod_timestamps_controller.rb
+++ app/modules/vod_reviews/controllers/vod_timestamps_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module Api
-  module V1
+module VodReviews
+  module Controllers
     class VodTimestampsController < Api::V1::BaseController
       before_action :set_vod_review, only: %i[index create]
       before_action :set_vod_timestamp, only: %i[update destroy]
