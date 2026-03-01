ac511ba19b08a18928388382d6979d86c00e5d65
Michael D. <contato@michaelbullet.com>
2026-03-01 08:20:01 -0300

    refactor(vod): move logic to modular architecture

--- app/controllers/api/v1/vod_reviews_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Proxy controller - inherits from modularized controller
-module Api
-  module V1
-    class VodReviewsController < ::VodReviews::Controllers::VodReviewsController
-    end
-  end
-end
