4ea0fbb42b612191d4cd5a36b46943b84abb3384
Michael D. <michael@prostaff.gg>
2026-04-09 21:43:32 -0300

    fix: solve arenaBR CORS issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # The fallback (second argument) must be a single string separated by commas
-    origins ENV.fetch('CORS_ORIGINS', 'http://localhost:5173,http://localhost:8888,http://localhost:4444,https://scrims.lol,https://prostaff.vercel.app,https://prostaff.gg,https://www.prostaff.gg,https://api.prostaff.gg,https://status.prostaff.gg,https://docs.prostaff.gg').split(',')
+    origins ENV.fetch('CORS_ORIGINS', 'http://localhost:3000,http://localhost:5173,http://localhost:8888,http://localhost:4444,https://scrims.lol,https://prostaff.vercel.app,https://prostaff.gg,https://www.prostaff.gg,https://api.prostaff.gg,https://status.prostaff.gg,https://docs.prostaff.gg,https://arenabr.gg,https://www.arenabr.gg').split(',')
 
     resource '*',
              headers: :any,
