856235ca12fd0fcc534cc638e9df8fed113c3b97
Michael D. <michael@prostaff.gg>
2026-05-08 07:44:31 -0300

    fix: solve stack trace audit

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -10,8 +10,6 @@ class SitemapController < ApplicationController
     @base_url = ENV.fetch('APP_URL', 'https://prostaff.gg')
     @current_time = Time.current.iso8601
 
-    respond_to do |format|
-      format.xml { render template: 'sitemap/index', layout: false }
-    end
+    render template: 'sitemap/index', layout: false, content_type: 'application/xml'
   end
 end
